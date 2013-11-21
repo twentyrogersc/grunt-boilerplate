4de8aa23e574fe2bf4a05c4219e3c402d7ff8e3b
Craig Rogers <20rogersc@gmail.com>
2013-11-21 12:18:12 +0000

    Added mkdir, removed config.

--- gruntfile.js
+++ gruntfile.js
@@ -1,13 +1,24 @@
-var config = require('./grunt.json')
+var config = {}
   , path = require('path')
 
 module.exports = function(grunt) {
 
-  Object.keys(config).forEach(function(key) {
-    config[key] = path.resolve(config[key])+'/'
-  })
+  config.assets = 'app'
+  config.css = config.assets+'/css'
+  config.sass = config.assets+'/scss'
+  config.js = config.assets+'/js'
+  config.bower = config.js+'/lib'
+
 
   grunt.initConfig({
+
+    mkdir: {
+      init: {
+        options: {
+          create: [config.css, config.sass, config.bower]
+        },
+      },
+    },
 
     sass: {
       production: {
@@ -57,27 +68,33 @@ module.exports = function(grunt) {
 
     clean: {
       bower: {
-        src: config.bower+'*',
+        src: config.bower+'/*',
         filter: 'isDirectory'
       }
     },
 
     watch: {
       sass: {
-        files: [config.sass+'*.scss'],
+        files: [config.sass+'/*.scss'],
         tasks: ['sass:dev']
+      },
+      bower: {
+        files: ['bower.json'],
+        tasks: ['bower:update']
       }
     }
 
   })
 
   grunt.loadNpmTasks('grunt-bower-task')
+  grunt.loadNpmTasks('grunt-mkdir')
   grunt.loadNpmTasks('grunt-contrib-clean')
   grunt.loadNpmTasks('grunt-contrib-sass')
   grunt.loadNpmTasks('grunt-contrib-uglify')
   grunt.loadNpmTasks('grunt-contrib-watch')
 
-  grunt.registerTask('default', ['bower:update', 'watch'])
+  grunt.registerTask('default', ['init', 'watch'])
+  grunt.registerTask('init', ['mkdir:init', 'bower:update'])
   grunt.registerTask('bower:update', ['bower:install', 'uglify:bower', 'clean:bower'])
   grunt.registerTask('production', ['bower:update', 'sass:production'])
 }
--- readme.md
+++ readme.md
@@ -3,16 +3,6 @@
 ## Installation
 ```sh
 $ npm install -g grunt-cli && npm install -g sass && npm install -g bower && npm install
-$ cp grunt.json.dist grunt.json # edit config as below
-```
-
-## Config
-```json
-{
-  "css": "path/to/compiled/scss/files",
-  "sass": "path/to/scss/files",
-  "bower": "path/to/place/js/libraries"
-}
 ```
 
 ## Run
