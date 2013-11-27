4447cc3b945a2ce99056b65d75d5f55ce34b73f0
Craig Rogers <20rogersc@gmail.com>
2013-11-27 11:07:01 +0000

    Moved out logic.

--- boilerplate.js
+++ boilerplate.js
@@ -1,15 +1,10 @@
 #! /usr/bin/env node
 
-var async = require('async')
+var args = require('./lib/args')
+  , async = require('async')
   , colors = require('colors')
-  , exec = require('child_process').exec
-  , fs = require('fs')
-  , mkdirp = require('mkdirp')
-  , mu = require('mu2')
-  , step = {}
-  , path = require('path')
-  , program = require('commander')
-
+  , files = require('./lib/files')
+  , install = require('./lib/install')
 
 // logging colors
 
@@ -19,103 +14,23 @@ colors.setTheme({
 })
 
 
-// parse args
-
-program
-  .usage('[options] <dir>')
-  .option('-n, --name <name>', 'name of the project')
-  .option('-a, --assets <path>', 'path to assets')
-  .parse(process.argv)
-
-if ( ! program.name) {
-  return console.error('Error'.error, 'No name specified (-n)')
-}
-
-if ( ! program.assets) {
-  return console.error('Error'.error, 'No assets path specified (-a)')
-}
-
-program.dir = path.resolve(program.args[0] || process.cwd())
-program.assets = path.resolve(program.dir, program.assets)
-
-if (program.dir.indexOf(__dirname) > -1) {
-  return console.error('Error'.error, 'Project root can not be set within this directory')
-}
-
-
-// install globals
-
-step.globals = function(cb) {
-  var globals = ['grunt-cli', 'sass', 'bower']
-    , npmInstall
-
-  npmInstall = function(module, next) {
-    exec('npm install -g '+module, next)
-  }
-
-  console.info('Installing'.ok, 'global dependencies')
-  async.forEachSeries(globals, npmInstall, cb)
-}
-
-
-// render/create files
-
-step.files = function(cb) {
-  var dir = path.join(__dirname, 'build')
-    , paths = { css: 'css', js: 'js', scss: 'scss', bower: 'js/lib' }
-    , renderAndMove
-
-  Object.keys(paths).forEach(function(key) {
-    paths[key] = path.join(program.assets, paths[key])+'/'
-    paths[key] = path.relative(program.dir, paths[key])+'/'
-  })
-
-  renderAndMove = function(file, next) {
-    var contents = []
-      , data = { name: program.name, paths: paths }
-      , filePath = path.join(dir, file)
-      , on = {}
-
-    on.data = function(partial) {
-      contents.push(partial.toString())
-    }
-
-    on.end = function() {
-      contents = contents.join('')
-      filePath = path.join(program.dir, file)
-      fs.writeFile(filePath, contents, next)
-    }
-
-    mu.compileAndRender(filePath, data)
-      .on('data', on.data)
-      .on('end', on.end)
-      .on('error', next)
-  }
-
-  console.info('Writing'.ok, 'package files')
-  fs.readdir(dir, function(err, files) {
-    mkdirp(program.dir, function() {
-      async.forEachSeries(files, renderAndMove, cb)
-    })
-  })
-}
-
-
-// install grunt dependencies
-
-step.grunt = function(cb) {
-  var cmds = [ 'npm install', 'grunt init' ]
-    , run
-
-  run = function(cmd, next) {
-    exec(cmd, { cwd: program.dir }, next)
-  }
-
-  console.info('Running'.ok, 'grunt')
-  async.forEachSeries(cmds, run, cb)
-}
-
-
 // go
 
-async.series([ step.globals, step.files, step.grunt ])
+args.parse(process.argv, function(err, config) {
+  if (err) return console.error('Error'.error, err)
+
+  async.series([
+    function(next) {
+      console.info('Installing'.ok, 'global dependencies')
+      install.globals(next)
+    },
+    function(next) {
+      console.info('Writing'.ok, 'package files')
+      files.render(config, next)
+    },
+    function(next) {
+      console.info('Running'.ok, 'grunt')
+      install.grunt(config.dir, next)
+    }
+  ])
+})
--- readme.md
+++ readme.md
@@ -1,5 +1,8 @@
-# grunt-boilerplate
+# grunt-boilerplate ([grunt-setup](https://npmjs.org/package/grunt-setup))
+Creates a [grunt](http://gruntjs.com) and [bower](http://bower.io) setup that gives you [sass](http://sass-lang.com), [bourbon](http://bourbon.io), [RequireJS](http://requirejs.org), [jQuery](http://jquery.com), [backbone.js](http://backbonejs.org) and [underscore.js](http://underscorejs.org/) out of the box.
 
+## Install & run
 ```sh
-node boilerplate.js -n "project-name" -a "assets/dir" project/root
+$ npm install -g grunt-setup
+$ grunt-setup -n "project-name" -a "assets/dir" project/root
 ```
